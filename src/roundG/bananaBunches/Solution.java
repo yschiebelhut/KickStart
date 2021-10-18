64af391916a187443278f4fc708b927da778aa78
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-10-18 09:45:01 +0200

    solution attempts for roundG in Kickstart

---
+++ src/roundG/bananaBunches/Solution.java
@@ -0,0 +1,24 @@
+package roundG.bananaBunches;
+
+import java.util.Scanner;
+
+/**
+ * @author Yannik Schiebelhut
+ */
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numTests = sc.nextInt();
+		for (int i = 0; i < numTests; i++) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+
+			int[] trees = new int[n];
+			for (int j = 0; j < n; j++) {
+				trees[i] = sc.nextInt();
+			}
+
+
+		}
+	}
+}
